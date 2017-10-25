e60d6c3889ca2a81a02dca3542d2def17c126188
subscribeken <subscribeken@subscribeken.com>
2017-10-25 17:45:55 -0400

    First commit

---
+++ SimpleMavenQuickStart/src/main/java/com/nkendyal/learning/maven/SimpleMavenQuickStart/App.java
@@ -0,0 +1,13 @@
+package com.nkendyal.learning.maven.SimpleMavenQuickStart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
