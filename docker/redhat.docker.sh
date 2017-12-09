303bde2966eed4001ee744dc0169c5836df2d17f
stacktrace <subscribeken@gmail.com>
2017-12-09 17:07:34 -0500

    Update redhat.docker.sh
    Updated instructions for RHEL Docker install

--- docker/redhat.docker.sh
+++ docker/redhat.docker.sh
@@ -1,15 +1,14 @@
+
 # pre-requisite for container-selinux-2.9-4.el7.noarch.rpm
 sudo yum install policycoreutils-python
 
+#Install selinux container
 wget ftp://ftp.icm.edu.pl/vol/rzm6/linux-centos-vault/7.3.1611/extras/x86_64/Packages/container-selinux-2.9-4.el7.noarch.rpm
 sudo rpm -i container-selinux-2.9-4.el7.noarch.rpm
 
 #Set up the Docker CE repository on RHEL:
 sudo yum install -y yum-utils
-sudo yum install -y device-mapper-persistent-data lvm2
-sudo yum-config-manager --enable rhel-7-server-extras-rpms
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum makecache fast
 
 # Install the latest version of Docker CE on RHEL:
 sudo yum -y install docker-ce
@@ -17,12 +16,12 @@ sudo yum -y install docker-ce
 #Start Docker:
 sudo systemctl start docker
 
-#Test your Docker CE installation:
-sudo docker run hello-world
-
 # configure Docker to start on boot
 sudo systemctl enable docker
 
 # add user to the docker group 
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+#Test your Docker CE installation:
+sudo docker run hello-world
